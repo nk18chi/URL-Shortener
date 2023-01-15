515e5c0db3f5bb9b846390446b82870558fcc726
Naoki Mita <naoki.mita18@gmail.com>
2023-01-15 13:46:34 -0800

    chore: add husky and lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //   "./client/**/*.{js,jsx,ts,tsx}": (fileName) => [`yarn run client:lint ${fileName.join(" ")}`, "tsc -p ./client/tsconfig.json --noEmit"],
+  "./server/**/*.ts": (fileName) => [`yarn run server:lint ${fileName.join(" ")}`, "tsc -p ./server/tsconfig.json --noEmit"],
+};
