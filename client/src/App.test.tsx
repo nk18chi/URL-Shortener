bc4c0ee4b786c7656595acc03f3f7e2fcba34234
Naoki Mita <naoki.mita18@gmail.com>
2023-01-17 13:17:00 -0800

    test: add App.test.tsx

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App.tsx", () => {
+  test("should show a headline", () => {
+    render(<App />);
+    expect(screen.getByText(/URL Shortener/i)).toBeInTheDocument();
+  });
+  test("should show a description", () => {
+    render(<App />);
+    expect(screen.getByText(/Generate a short url and redirect to the long url when a user click the short url./i)).toBeInTheDocument();
+  });
+  test("should render ShortUrlInput component", () => {
+    render(<App />);
+    expect(screen.getByTestId("short-url-input")).toBeInTheDocument();
+  });
 });
